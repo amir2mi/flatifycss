5be0e5c59b2959cfd72b2dbb500740b678402a41
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-10-29 00:43:50 +0330

    Installed Docusaurus in gh-pages branch

---
+++ docs/assets/js/88fcc580.d349c151.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[430],{8595:function(s){s.exports=JSON.parse('{"allTagsPath":"/FlatifyCSS/blog/tags","slug":"/FlatifyCSS/blog/tags/docusaurus","name":"docusaurus","count":4,"permalink":"/FlatifyCSS/blog/tags/docusaurus"}')}}]);
