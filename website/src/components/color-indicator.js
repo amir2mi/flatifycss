cd86f7fc0ed54bbfc1cb8343489a9261ec8aef07
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-10-30 20:21:44 +0330

    Custom accent color for blockquote

--- website/src/components/color-indicator.js
+++ website/src/components/color-indicator.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ColorIndicator({ color, title, inline }) {
 	return (
-		<div className={`docus-color-indicator flex-center-y ${inline ? 'inline':''}`}>
+		<div className={`docus-color-indicator flex-center-y ${inline ? "inline" : ""}`}>
 			<div className="docus-color" style={{ backgroundColor: color }} />
 			<code>{title}</code>
 		</div>
