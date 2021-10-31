e57c06a9a0a4c0a93978b13cacd5fbcaff607895
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-11-01 02:18:50 +0330

    Built website last version

--- docs/assets/js/878e85e1.73c6b90b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[843],{8648:function(e){e.exports=JSON.parse('{"permalink":"/FlatifyCSS/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
