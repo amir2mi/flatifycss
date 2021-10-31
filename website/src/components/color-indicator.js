b66a2f5c007107817180cf3f61e7fc5094abad09
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-10-31 13:00:56 +0330

    Added figure overlay captions and source

--- website/src/components/color-indicator.js
+++ website/src/components/color-indicator.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function ColorIndicator({ color, title, inline }) {
+export default function ColorIndicator({ color, title, inline, style, className }) {
 	return (
 		<div className={`docus-color-indicator flex-center-y ${inline ? "inline" : ""}`}>
-			<div className="docus-color" style={{ backgroundColor: color }} />
+			<div
+				className={`docus-color${className ? " " + className : ""}`}
+				style={{ backgroundColor: color, ...style }}
+			/>
 			<code>{title}</code>
 		</div>
 	);
