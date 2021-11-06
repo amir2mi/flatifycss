02e2054d3d6cd28c965e3f7f82fee581710d6f30
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-11-06 15:10:59 +0330

    Added logger for alert error and reduced animation for alert will be removed

--- js/components/alert.js
+++ js/components/alert.js
@@ -4,13 +4,21 @@ import logger from "../utils/logger";
 
 document.addEventListener("DOMContentLoaded", () => {
 	$(document).on("click", ".close-button", function () {
-		this.closest(".alert").classList.add("alert-will-be-removed");
+		const wrapper = this.closest(".alert");
 
-		logger("info", "Alert will be removed...", this);
+		// return if close button does not have wrapper with [.alert] class
+		if (!wrapper) {
+			return logger("error", `Close button is not in an ".alert"`, this.parentElement);
+		} else {
+			logger("info", "Alert will be removed...", this);
+		}
 
+		// add animation class to remove the alert
+		wrapper.classList.add("alert-will-be-removed");
+
+		// after delay remove alert from DOM
 		setTimeout(() => {
 			this.closest(".alert").remove();
-
 			logger("info", `Alert is removed after ${config.alertRemoveAnimationDuration}ms delay.`, this);
 		}, config.alertRemoveAnimationDuration);
 	});
--- js/utils/logger.js
+++ js/utils/logger.js
@@ -1,5 +1,13 @@
 import config from "../config";
 
+/**
+ * Log a message in the console or another services,
+ * It will log if [enableLogging] is true in config.json.
+ *
+ * @param type {string} console logging type.
+ * @param message {string} console logging type.
+ * @param data {any} Attached data for further information.
+ */
 export default function logger(type, message, data) {
 	if (config.enableLogging === true) {
 		switch (type) {
