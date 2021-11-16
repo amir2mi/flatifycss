894a6fe47ae0b26f42bbe3b450acd5ec6a792467
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-11-16 16:43:57 +0330

    Added modal-backdrop event to hide modal and required modal type to repeat the showing  animation

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,11 +1,49 @@
 import $ from "../utils/vent";
 import logger from "../utils/logger";
 
+function showModal(modal, modalBackdrop) {
+	if (modalBackdrop) {
+		// there is a backdrop for modal, display it
+		modalBackdrop.classList.add("show");
+
+		logger("info", "Modal backdrop is displayed", modal);
+	} else {
+		// otherwise create one
+		let backdropElement = document.createElement("div");
+		backdropElement.classList.add("backdrop-layer", "modal-backdrop", "show");
+		document.body.appendChild(backdropElement);
+
+		logger("info", "Modal backdrop is created and shown", modal);
+	}
+
+	// change aria-modal to true
+	modal.ariaModal = true;
+	// add [display: block] and animation class
+	modal.classList.add("show");
+
+	logger("info", "Modal is shown", modal);
+}
+
+function hideModal(modal, modalBackdrop) {
+	modalBackdrop.classList.add("backdrop-will-be-removed");
+	modal.classList.add("modal-will-be-removed");
+
+	// after delay hide modal and its backdrop
+	$(modal).once("animationend", (e) => {
+		// removed hide animation class and show [display:none]
+		modalBackdrop.classList.remove("show", "backdrop-will-be-removed");
+		modal.classList.remove("show", "modal-will-be-removed");
+
+		// change aria-modal to false
+		modal.ariaModal = false;
+	});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	$(document).on("click", ".open-modal", function (e) {
 		let modal;
 		const target = e.target.getAttribute("data-modal-target");
-		const backdrop = document.querySelector(".modal-backdrop");
+		const modalBackdrop = document.querySelector(".modal-backdrop");
 
 		if (target && document.querySelector(target)) {
 			modal = document.querySelector(target);
@@ -17,23 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			);
 		}
 
-		if (backdrop) {
-			// there is a backdrop for modal, display it
-			backdrop.classList.add("show");
-
-			logger("info", "Modal backdrop is displayed", modal);
-		} else {
-			// otherwise create one
-			let backdropElement = document.createElement("div");
-			backdropElement.classList.add("backdrop-layer", "modal-backdrop", "show");
-			document.body.appendChild(backdropElement);
-
-			logger("info", "Modal backdrop is created and shown", modal);
-		}
-
-		modal.classList.add("show");
-
-		logger("info", "Modal is shown", modal);
+		showModal(modal, modalBackdrop);
 	});
 
 	$(document).on("click", ".close-modal", function (e) {
@@ -49,13 +71,21 @@ document.addEventListener("DOMContentLoaded", () => {
 			if (!modal) return;
 		}
 
-		modalBackdrop.classList.add("backdrop-will-be-removed");
-		modal.classList.add("modal-will-be-removed");
+		hideModal(modal, modalBackdrop);
+	});
 
-		// after delay hide modal and its backdrop
-		$(modal).once("animationend", (e) => {
-			modalBackdrop.classList.remove("show", "backdrop-will-be-removed");
-			modal.classList.remove("show", "modal-will-be-removed");
-		});
+	$(document).on("click", ".modal-backdrop", function (e) {
+		const modal = document.querySelector(".modal.show");
+		if (!modal) return logger("warn", "There is no modal in this page to hide", modal);
+
+		// if modal is required repeat the show animation else hide it.
+		if (modal.classList.contains("required")) {
+			modal.classList.remove("show");
+			setTimeout(() => {
+				modal.classList.add("show");
+			}, 150);
+		} else {
+			hideModal(modal, e.target);
+		}
 	});
 });
