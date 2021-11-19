13477fa47568ed189f12087e66e27fa6a07779a6
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-11-19 23:58:10 +0330

    Revert "Added close of click outside of the dropdown area"
    This reverts commit c0581a0ba25fc5e04d20531de7962765ea9891e0.

--- js/components/dropdown.js
+++ js/components/dropdown.js
@@ -27,51 +27,26 @@ function hideDropdown(dropdown, toggle) {
 	});
 }
 
-function findDropdownToggle(dropdown) {
-	// get dropdown wrapper if wrapper was not found return an error
-	const wrapper = dropdown.closest(".dropdown-wrapper");
-	if (!wrapper) return logger("error", "Dropdown should be inside '.dropdown-wrapper'");
-
-	return wrapper.querySelector(".dropdown-toggle");
-}
-
 document.addEventListener("DOMContentLoaded", () => {
 	$(document).on("click", ".dropdown-toggle", function (e) {
 		const wrapper = this.closest(".dropdown-wrapper");
+		const toggle = e.target;
 
 		// return if dropdown toggle does not have wrapper with [.dropdown-wrapper] class
-		if (!wrapper) return logger("error", "Dropdown toggle button should be inside '.dropdown-wrapper'");
+		if (!wrapper) return logger("error", "Dropdown toggle button should be inside '.dropdown-wrapper' wrapper");
 
 		const dropdown = wrapper.querySelector(".dropdown");
 		// return if there is not dropdown inside dropdown wrapper
-		if (!dropdown) return logger("error", "Could not found '.dropdown' element inside '.dropdown-wrapper'");
+		if (!dropdown) return logger("error", "Could not found '.dropdown' element inside '.dropdown-wrapper' wrapper");
 
 		if (dropdown.classList.contains("show")) {
-			hideDropdown(dropdown, this);
+			hideDropdown(dropdown, toggle);
 		} else {
-			showDropdown(dropdown, this);
+			showDropdown(dropdown, toggle);
 		}
 	});
 
-	$(document).on("click", function (e) {
-		// select active dropdowns which has [data-dropdown-auto-close] = outside || true || default.
-		let activeDropdowns = document.querySelectorAll(
-			".dropdown.show:not([data-dropdown-auto-close]), .dropdown.show[data-dropdown-auto-close='outside'], .dropdown.show[data-dropdown-auto-close='true']"
-		);
-		console.log(activeDropdowns);
-		// if there is no active dropdown return
-		if (!activeDropdowns) return;
-
-		activeDropdowns.forEach((dropdown) => {
-			const toggle = findDropdownToggle(dropdown);
-
-			/** hide dropdown if:
-			 * 1- the clicked target IS NOT dropdown itself.
-			 * 2- the dropdown toggle button IS NOT the clicked target.
-			 */
-			if (e.target.closest(".dropdown") !== dropdown && e.target !== toggle) {
-				hideDropdown(dropdown, toggle);
-			}
-		});
-	});
+    $(document).on("click", function () {
+    
+    });
 });
