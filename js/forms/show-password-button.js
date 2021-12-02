60ffb9e5fab12420c5d3bc7dc40880a5cbd7c2e8
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2021-12-02 22:48:42 +0330

    Form inputs - Toggle password visibility JS codes improved

--- js/forms/show-password-button.js
+++ js/forms/show-password-button.js
@@ -7,28 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
 		e.preventDefault();
 
 		const wrapper = this.closest(".input-wrapper");
-
 		// return if show password button does not have wrapper with [.input-wrapper] class
 		if (!wrapper) {
-			return logger("error", `Show password button is not in an ".input-wrapper"`, this.parentElement);
+			return logger("error", "Show password button is not in an '.input-wrapper'", this.parentElement);
 		}
 
-		if (wrapper.classList.contains("visible-password")) {
-			// remove visible class for eye icon
-			wrapper.classList.remove("visible-password");
-			// change input type to password
-			const input = wrapper.querySelector("input");
-			if (input) input.type = "password";
-
-			logger("info", "Password is hidden", input);
-		} else {
-			// add visible class for eye icon
-			wrapper.classList.add("visible-password");
-			// change input type to text
-			const input = wrapper.querySelector("input");
-			if (input) input.type = "text";
-
-			logger("info", "Password is shown", input);
+		const input = wrapper.querySelector("input");
+		// return if show password button does not have wrapper with [.input-wrapper] class
+		if (!input) {
+			return logger("error", "Show password button should be inside '.input-wrapper' with input type password", this.parentElement);
 		}
+
+		wrapper.classList.toggle("visible-password");
+		input.type = wrapper.classList.contains("visible-password") ? "password" : "text";
 	});
 });
